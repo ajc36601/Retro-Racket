0254ecc701461f6395d7b44214a583b79a38a668
ajc36601 <aj_copenhaver7536@yahoo.com>
2024-01-12 14:44:21 -0500

    Ball script

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -1,7 +1,59 @@
 extends CharacterBody2D
 
+var state = "bouncing"
 
+var zVelocity = -100
+var gravity = 125
+
+@onready var sprite = get_node("sprite")
 
 func _physics_process(delta):
+	print(sprite.position.y)
+	print(zVelocity)
+	match state:
+		"bouncing":
+			if sprite.position.y >= -1 && zVelocity > 0:
+				sprite.position.y = -1
+				if zVelocity >= -10:
+					zVelocity = 0
+					changeState("still")
+				else:
+					zVelocity = -0.7*zVelocity
+			sprite.move_local_y(zVelocity*delta)
+			zVelocity = zVelocity+gravity*delta
+		"rolling":
+			pass
+		"still":
+			pass
+		"preserve":
+			pass
+		"serve":
+			pass
 	move_and_slide()
-	velocity = Vector2(0,-2)
+
+func changeState(desiredState):
+	match state:
+		"bouncing":
+			pass
+		"rolling":
+			pass
+		"still":
+			pass
+		"preserve":
+			pass
+		"serve":
+			pass
+	
+	match desiredState:
+		"bouncing":
+			pass
+		"rolling":
+			pass
+		"still":
+			pass
+		"preserve":
+			pass
+		"serve":
+			pass
+	
+	state = desiredState
